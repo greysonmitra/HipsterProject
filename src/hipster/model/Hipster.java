305318698bfd798a60605871ac1c46de9c4bcf0e
greysonmitra <greyson.mitra@outlook.com>
2015-11-19 08:23:51 -0700

    Made a book array. It has a few good books' info in it.

--- src/hipster/model/Hipster.java
+++ src/hipster/model/Hipster.java
@@ -4,14 +4,15 @@ public class Hipster
 {
 	private String name;
 	private String [] hipsterPhrases;
-//  private Book [] hipsterBooks;
+	private Book [] hipsterBooks;
 	
 	public Hipster()
 	{
 		this.name = "";
 		this.hipsterPhrases = new String[5];
+		this.hipsterBooks = new Book[3];
 		setupArray();
-//		setupBooks();
+		setupBooks();
 	}
 	
 	private void setupArray()
@@ -23,14 +24,34 @@ public class Hipster
 		hipsterPhrases[4] = "I actually listen to good music like Sir mixalot unlike people from my generation who listen to crap like Guy Fieri";
 	}
 	
-	private void setupbooks()
+	private void setupBooks()
 	{
+		Book firstBook, secondBook, thirdBook;
+		firstBook = new Book();
+		firstBook.setAuthor("Neal Stephenson");
+		firstBook.setTitle("Diamonds in the rough");
+		firstBook.setSubject("Autobiography");
+		firstBook.setPageCount(6000);
+		firstBook.setPrice(89.99);
 		
+		secondBook = new Book(361, "Rick Riordan", "The Last Olympian", "Fantasy", 19.99);
+		
+		
+		thirdBook = new Book();
+		thirdBook.setAuthor("Shaun Weasley");
+		thirdBook.setTitle("Mastering the 'Magic' of Sandbagging");
+		thirdBook.setSubject("Documentary");
+		thirdBook.setPageCount(34696);
+		thirdBook.setPrice(120);
+		
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = secondBook;
+		hipsterBooks[2] = thirdBook;
 	}
 	
 	public Hipster(String name)
 	{
-		
+		this.name = name;
 	}
 	
 	public String getName()
